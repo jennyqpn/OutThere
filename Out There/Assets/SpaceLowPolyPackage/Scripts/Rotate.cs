6538a02dc638ed75bfe8874bfbc77766f8a2a530
Jenny Nguyen <jennyquynhphuong@gmail.com>
2020-03-02 21:15:42 -0600

    finished farm, started town + wilderness terrain

---
+++ Out There/Assets/SpaceLowPolyPackage/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public float speed = 1;
+	public bool right;
+
+	// Update is called once per frame
+	void Update () {
+		if (right) {
+			transform.Rotate (Vector3.right * Time.deltaTime * speed);
+		} else {
+			transform.Rotate (Vector3.up * Time.deltaTime * speed);
+		}
+	}
+}
