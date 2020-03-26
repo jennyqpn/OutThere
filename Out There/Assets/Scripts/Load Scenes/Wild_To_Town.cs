b271195ee477c314cf4544335c7b3d2fb13df202
Jenny Nguyen <jennyquynhphuong@gmail.com>
2020-03-25 23:55:06 -0500

    loading between scenes work

---
+++ Out There/Assets/Scripts/Load Scenes/Wild_To_Town.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wild_To_Town : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+
+        if(col.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Town");
+        }
+    }
+}
