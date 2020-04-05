277444e2a4fb257dc6a92085f3801aef9015b949
Leah Domas <c00227185@louisiana.edu>
2020-04-05 17:39:33 -0500

    Finished functionality of title screen

---
+++ Out There/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    public Dropdown resolutonDropdown;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutonDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentRes = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentRes = i;
+            }
+        }
+
+        resolutonDropdown.AddOptions(options);
+        resolutonDropdown.value = currentRes;
+        resolutonDropdown.RefreshShownValue();
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void setRes(int resIndex)
+    {
+        Resolution res = resolutions[resIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+}
