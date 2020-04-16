24fe42a7fb5f991ab8342014ec43fb2dc08d65d5
Leah Domas <c00227185@louisiana.edu>
2020-04-15 20:31:15 -0500

    Credits screen finished

---
+++ Out There/Assets/Scripts/Title Screens/Scroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scroll : MonoBehaviour
+{
+    // Update is called once per frame
+    public float scrollSpeed = 15;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 loclVectorUp = transform.TransformDirection(0, 1, 0);
+        pos += loclVectorUp * scrollSpeed * Time.deltaTime;
+        transform.position = pos;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Title Screen");
+        }
+    }
+}
