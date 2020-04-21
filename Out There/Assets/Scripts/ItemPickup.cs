89aa41f42ef756aeca75868851ce24376b828ae2
Leah Domas <c00227185@louisiana.edu>
2020-04-21 11:32:45 -0500

    Adds back wilderness

---
+++ Out There/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Tools")
+        {
+            Debug.Log("Picked up");
+            //add to inventory
+            Destroy(col.gameObject);
+        }
+    }
+}
