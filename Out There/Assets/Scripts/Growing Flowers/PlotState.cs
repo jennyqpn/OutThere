caf01aff58f1472888eacd1050b3ab9c4a2ed76e
C00241011 <c00241011@louisiana.edu>
2020-04-22 15:31:03 -0500

    Plot information can be passed between wilderness and farm.

--- Out There/Assets/Scripts/Growing Flowers/PlotState.cs
+++ Out There/Assets/Scripts/Growing Flowers/PlotState.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlotState : MonoBehaviour
 {
 	public bool canGrow;
-	public bool canPick;
+	public bool nextStage;
 	GameObject player;
+	GameObject saveData;
+	SaveData.plotInfo[,] plotList;
 	string flowerNum;
+	public int plotX;
+	public int plotY;
 
 	//Plot Type
 	public enum PlotType { Empty, Flower1, Flower2, Flower3, Flower4, Flower5, Flower6, Flower7 }
@@ -21,22 +25,25 @@ public class PlotState : MonoBehaviour
 	void Start()
     {
 		player = GameObject.FindGameObjectWithTag("Player");
+		saveData = GameObject.FindGameObjectWithTag("Save Data");
 		canGrow = false;
-		canPick = false;
 		flowerNum = "Empty";
-    }
+		nextStage = true;
+		plotList = SaveData.plotList;
 
+	}
     // Update is called once per frame
     void Update()
     {
+		plotType = plotList[plotX - 1, plotY - 1].plotType;
+		plantGrowth = plotList[plotX - 1, plotY - 1].plantGrowth;
 		Holding.Flowers flower = player.GetComponent<Holding>().flowers;
 		if (canGrow)//If the player is on top of the plot and it is empty or has a grown flower on it
 		{
-			if (canPick)//If the plot has a grown flower on it
+			if (plantGrowth == PlantGrowth.Grown)//If the plot has a grown flower on it
 			{
 				if (Input.GetMouseButtonDown(0) && canGrow)
 				{
-					canPick = false;
 					GrownToEmpty(flowerNum);
 				}
 			}
@@ -49,50 +56,43 @@ public class PlotState : MonoBehaviour
 
 				switch (flower) {
 					case Holding.Flowers.Flower1:
-						plotType = PlotType.Flower1;
-						plantGrowth = PlantGrowth.Seed;
+						plotList[plotX - 1, plotY - 1].plotType = PlotType.Flower1;
 						Debug.Log("1");
 						flowerNum = "1";
 						EmptyToSeed(flowerNum);
 						break;
 					case Holding.Flowers.Flower2:
-						plotType = PlotType.Flower2;
-						plantGrowth = PlantGrowth.Seed;
+						plotList[plotX - 1, plotY - 1].plotType = PlotType.Flower2;
 						Debug.Log("2");
 						flowerNum = "2";
 						EmptyToSeed(flowerNum);
 						break;
 					case Holding.Flowers.Flower3:
-						plotType = PlotType.Flower3;
-						plantGrowth = PlantGrowth.Seed;
+						plotList[plotX - 1, plotY - 1].plotType = PlotType.Flower3;
 						Debug.Log("3");
 						flowerNum = "3";
 						EmptyToSeed(flowerNum);
 						break;
 					case Holding.Flowers.Flower4:
-						plotType = PlotType.Flower4;
-						plantGrowth = PlantGrowth.Seed;
+						plotList[plotX - 1, plotY - 1].plotType = PlotType.Flower4;
 						Debug.Log("4");
 						flowerNum = "4";
 						EmptyToSeed(flowerNum);
 						break;
 					case Holding.Flowers.Flower5:
-						plotType = PlotType.Flower5;
-						plantGrowth = PlantGrowth.Seed;
+						plotList[plotX - 1, plotY - 1].plotType = PlotType.Flower5;
 						Debug.Log("5");
 						flowerNum = "5";
 						EmptyToSeed(flowerNum);
 						break;
 					case Holding.Flowers.Flower6:
-						plotType = PlotType.Flower6;
-						plantGrowth = PlantGrowth.Seed;
+						plotList[plotX - 1, plotY - 1].plotType = PlotType.Flower6;
 						Debug.Log("6");
 						flowerNum = "6";
 						EmptyToSeed(flowerNum);
 						break;
 					case Holding.Flowers.Flower7:
-						plotType = PlotType.Flower7;
-						plantGrowth = PlantGrowth.Seed;
+						plotList[plotX - 1, plotY - 1].plotType = PlotType.Flower7;
 						Debug.Log("7");
 						flowerNum = "7";
 						EmptyToSeed(flowerNum);
@@ -105,20 +105,26 @@ public class PlotState : MonoBehaviour
 			}
 		}
 
-		switch (plantGrowth)
+		if (nextStage)
 		{
-			case PlantGrowth.Seed:
-				StartCoroutine(SeedToGrowing(flowerNum));
-				break;
-			case PlantGrowth.Growing:
-				StartCoroutine(GrowingToGrown(flowerNum));
-				break;
+			switch (plantGrowth)
+			{
+				case PlantGrowth.Seed:
+					Debug.Log("Seed");
+					StartCoroutine(SeedToGrowing(flowerNum));
+					break;
+				case PlantGrowth.Growing:
+					Debug.Log("Growing");
+					StartCoroutine(GrowingToGrown(flowerNum));
+					break;
+			}
 		}
+
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if(plotType == PlotType.Empty || canPick)
+		if(plotType == PlotType.Empty || plantGrowth == PlantGrowth.Grown)
 		{
 			canGrow = true;
 		}
@@ -133,31 +139,34 @@ public class PlotState : MonoBehaviour
 	{
 		gameObject.transform.Find("flower" + num + "Seed").gameObject.SetActive(true);
 		gameObject.transform.Find("Empty Plot").gameObject.SetActive(false);
-		plantGrowth = PlantGrowth.Seed;
+		plotList[plotX - 1, plotY - 1].plantGrowth = PlantGrowth.Seed;
 	}
 
 	IEnumerator SeedToGrowing(string num)
 	{
+		nextStage = false;
 		yield return new WaitForSeconds(5);
 		gameObject.transform.Find("flower" + num + "Growing").gameObject.SetActive(true);
 		gameObject.transform.Find("flower" + num + "Seed").gameObject.SetActive(false);
-		plantGrowth = PlantGrowth.Growing;
+		plotList[plotX - 1, plotY - 1].plantGrowth = PlantGrowth.Growing;
+		nextStage = true;
 	}
 
 	IEnumerator GrowingToGrown(string num)
 	{
+		nextStage = false;
 		yield return new WaitForSeconds(5);
 		gameObject.transform.Find("flower" + num + "Grown").gameObject.SetActive(true);
 		gameObject.transform.Find("flower" + num + "Growing").gameObject.SetActive(false);
-		plantGrowth = PlantGrowth.Grown;
-		canPick = true;
+		plotList[plotX - 1, plotY - 1].plantGrowth = PlantGrowth.Grown;
+		nextStage = true;
 	}
 
 	void GrownToEmpty(string num)
 	{
 		gameObject.transform.Find("Empty Plot").gameObject.SetActive(true);
 		gameObject.transform.Find("flower" + num + "Grown").gameObject.SetActive(false);
-		plotType = PlotType.Empty;
-		plantGrowth = PlantGrowth.Empty;
+		plotList[plotX - 1, plotY - 1].plotType = PlotType.Empty;
+		plotList[plotX - 1, plotY - 1].plantGrowth = PlantGrowth.Empty;
 	}
 }
--- Out There/Assets/Scripts/Growing Flowers/SaveData.cs
+++ Out There/Assets/Scripts/Growing Flowers/SaveData.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaveData : MonoBehaviour
 {
+	public struct plotInfo
+	{
+		public PlotState.PlotType plotType;
+		public PlotState.PlantGrowth plantGrowth;
+	}
+
+	public static plotInfo[,] plotList = new plotInfo[5, 5];
+	public static string lastScene;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		if (SceneManager.GetActiveScene().name == "Farm")
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					plotList[i, j].plotType = PlotState.PlotType.Empty;
+					plotList[i, j].plantGrowth = PlotState.PlantGrowth.Empty;
+				}
+			}
+		}
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
+		string str = "";
+        foreach(plotInfo info in plotList)
+		{
+			str += "[" + info.plotType + "," + info.plantGrowth + "] ";
+		}
+		Debug.Log(str + "\n");
     }
 }
