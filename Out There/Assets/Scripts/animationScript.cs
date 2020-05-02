6219e99e511834a89766553c33c5a44d4bd31a33
Jordan <c00082940@gmail.com>
2020-05-02 01:00:47 -0500

    animation station

---
+++ Out There/Assets/Scripts/animationScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class animationScript : MonoBehaviour
+{
+    public Animator player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.SetBool("walking", true);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            player.SetBool("run", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            player.SetBool("walking", false);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            player.SetBool("run", false);
+        }
+
+
+    }
+}
