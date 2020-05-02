43a3b87af9e0f78d416b6770f331fa872ce33e76
C00241011 <c00241011@louisiana.edu>
2020-05-02 18:54:45 -0500

    Script Commit

---
+++ Out There/Assets/Scripts/ResetItems.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetItems : MonoBehaviour
+{
+	public List<Item> items;
+	// Start is called before the first frame update
+	void Start()
+    {
+		foreach (Item i in items)
+		{
+			i.quantity = 0;
+		}
+		Inventory.items = new List<Item>();
+	}
+}
