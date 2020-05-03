57d97df11053d8f8e274a6581d0baecfb71aea2b
C00241011 <c00241011@louisiana.edu>
2020-05-02 20:31:43 -0500

    Updated pause menu text and made scroll scene go to main menu

--- Out There/Assets/Scripts/Title Screens/Scroll.cs
+++ Out There/Assets/Scripts/Title Screens/Scroll.cs
@@ -5,20 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class Scroll : MonoBehaviour
 {
-    // Update is called once per frame
-    public float scrollSpeed = 15;
+	// Update is called once per frame
+	public float scrollSpeed = 15;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 pos = transform.position;
-        Vector3 loclVectorUp = transform.TransformDirection(0, 1, 0);
-        pos += loclVectorUp * scrollSpeed * Time.deltaTime;
-        transform.position = pos;
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 pos = transform.position;
+		Vector3 loclVectorUp = transform.TransformDirection(0, 1, 0);
+		pos += loclVectorUp * scrollSpeed * Time.deltaTime;
+		transform.position = pos;
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene("Title Screen");
-        }
-    }
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			SceneManager.LoadScene(0);//Load Title Screen
+		}
+	}
 }
